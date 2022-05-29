663133b4dc019a90f5975f807c56e4e351079c18
farhdibehnamdev <behnammfarhadi@gmail.com>
2022-05-29 14:38:32 +0430

    💄 Added pseduo class for menus.

--- js/script.js
+++ js/script.js
@@ -2,14 +2,18 @@
 const menu = document.querySelector(".menu");
 const menus = document.querySelectorAll(".menu li");
 
-const addActiveClass = function (e) {
-  menus.forEach((menu) => menu.classList.remove("active-menu"));
+const toggleActiveClass = function (e) {
+  menus.forEach((menu) => {
+    menu.classList.remove("active-menu");
+    menu.classList.add("hide-before");
+  });
   const currentSelectedElement = e.target.className;
   menus.forEach((menu) => {
     if (menu.className === currentSelectedElement) {
       e.target.classList.add("active-menu");
+      e.target.classList.remove("hide-before");
     }
   });
 };
 
-menu.addEventListener("click", addActiveClass);
+menu.addEventListener("click", toggleActiveClass);
