408ee080a9090a076222c040ede2a581214f5962
farhdibehnamdev <behnammfarhadi@gmail.com>
2022-06-07 17:56:01 +0430

    ♻️ refactor : Refactored my own state-based ui.
    I changed my own state-based ui by redux state management.

--- js/task.js
+++ js/task.js
@@ -1,21 +1,20 @@
-import { addTask } from "../redux/actions/index.js";
 import store from "../redux/store/index.js";
+import { addTask } from "../redux/actions/index.js";
 const generatedId = function () {
   return (
     Math.random().toString(36).substring(2) + new Date().getTime().toString(36)
   );
 };
-const init = function () {
+const TaskComponent = function () {
   console.log(generatedId());
   return function () {
     const template = function () {
-      let html = "";
       let state = store.getState();
-      console.log("state : ", state);
+
+      let html = "";
+      console.log("state: ", state);
       const flatData = _.values(state);
-      console.log("flatty:", flatData);
       let taskList = flatData.map((data, index) => {
-        console.log("data ::", data);
         return _.values(data).map((tsk) => {
           const { id, task } = tsk;
           return `
@@ -35,30 +34,28 @@ const init = function () {
       });
 
       if (taskList.length > 0) {
-        console.log("tttttt:", taskList);
-        const bag = taskList.join("");
-        console.log("tasdkkkk", bag);
-        html += `<ul class="tasks"> ${taskList} </ul>`;
+        let joined = taskList.join("").replaceAll(",", "");
+        html += `<ul class="tasks"> ${joined} </ul>`;
       }
 
       return html;
     };
 
-    let emitEvent = function (elem, detail) {
-      let event = new CustomEvent("render", {
-        bubbles: true,
-        cancelable: true,
-        detail: detail || {},
-      });
-      elem.dispatchEvent(event);
-    };
+    // let emitEvent = function (elem, detail) {
+    //   let event = new CustomEvent("render", {
+    //     bubbles: true,
+    //     cancelable: true,
+    //     detail: detail || {},
+    //   });
+    //   elem.dispatchEvent(event);
+    // };
 
-    let render = function () {
-      let app = document.querySelector("#app");
-      if (!app) return;
-      app.innerHTML = template();
-      emitEvent(app, null);
-    };
+    // let render = function () {
+    //   let app = document.querySelector("#app");
+    //   if (!app) return;
+    //   app.innerHTML = template();
+    //   // emitEvent(app, null);
+    // };
 
     let submitHandler = function (event) {
       if (!event.target.matches("#add-task-input")) return;
@@ -67,7 +64,6 @@ const init = function () {
       let task = event.target.querySelector(".task-input");
       if (!task || task.value.length < 1) return;
 
-      // data.tasks.push(task.value);
       const id = generatedId();
       const newTask = {
         id,
@@ -82,17 +78,16 @@ const init = function () {
 
       store.dispatch(addTask(id, newTask));
 
-      // Object.assign(data.tasks, task.value);
-      render();
-
       task.value = "";
       task.focus();
     };
-
     document.addEventListener("submit", submitHandler, false);
     // document.addEventListener("render", renderHandler, false);
+    let app = document.querySelector("#app");
+    store.subscribe(() => {
+      app.innerHTML = template();
+    });
   };
 };
-const render = init();
+const render = TaskComponent();
 render();
-store.subscribe(render);
