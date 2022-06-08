19145f8e5bf76789acc14135995be86b0f4cee09
farhdibehnamdev <behnammfarhadi@gmail.com>
2022-06-08 14:42:23 +0430

    ♻️ refactore : Refactored clsoure function to IIFE pattern.

--- js/task.js
+++ js/task.js
@@ -1,93 +1,62 @@
 import store from "../redux/store/index.js";
 import { addTask } from "../redux/actions/index.js";
-const generatedId = function () {
-  return (
-    Math.random().toString(36).substring(2) + new Date().getTime().toString(36)
-  );
-};
-const TaskComponent = function () {
-  console.log(generatedId());
-  return function () {
-    const template = function () {
-      let state = store.getState();
+import { generatedId } from "../libs/utils.js";
 
-      let html = "";
-      console.log("state: ", state);
-      const flatData = _.values(state);
-      let taskList = flatData.map((data, index) => {
-        return _.values(data).map((tsk) => {
-          const { id, task } = tsk;
-          return `
-        <li class="task" data-id=${task.id}>
-             <p class="has-task">
-               <span class="check-task-name">
-                 <i class="ph-circle-bold icon-task-size"></i>
-                 <span>${task.title}</span>
-               </span>
-               <span class="important-task">
-                 <i class="ph-star-bold icon-task-size"></i>
-               </span>
-             </p>
-           </li>
-           `;
-        });
-      });
+(function () {
+  let submitHandler = function (event) {
+    if (!event.target.matches("#add-task-input")) return;
+    event.preventDefault();
 
-      if (taskList.length > 0) {
-        let joined = taskList.join("").replaceAll(",", "");
-        html += `<ul class="tasks"> ${joined} </ul>`;
-      }
+    let task = event.target.querySelector(".task-input");
+    if (!task || task.value.length < 1) return;
 
-      return html;
+    const id = generatedId();
+    const newTask = {
+      id,
+      title: task.value,
+      createdDate: Date.now(),
+      important: false,
+      completed: false,
+      isOverDated: false,
+      planned: false,
+      myDay: false,
     };
 
-    // let emitEvent = function (elem, detail) {
-    //   let event = new CustomEvent("render", {
-    //     bubbles: true,
-    //     cancelable: true,
-    //     detail: detail || {},
-    //   });
-    //   elem.dispatchEvent(event);
-    // };
+    store.dispatch(addTask(id, newTask));
 
-    // let render = function () {
-    //   let app = document.querySelector("#app");
-    //   if (!app) return;
-    //   app.innerHTML = template();
-    //   // emitEvent(app, null);
-    // };
-
-    let submitHandler = function (event) {
-      if (!event.target.matches("#add-task-input")) return;
-      event.preventDefault();
-
-      let task = event.target.querySelector(".task-input");
-      if (!task || task.value.length < 1) return;
-
-      const id = generatedId();
-      const newTask = {
-        id,
-        title: task.value,
-        createdDate: Date.now(),
-        important: false,
-        completed: false,
-        isOverDated: false,
-        planned: false,
-        myDay: false,
-      };
-
-      store.dispatch(addTask(id, newTask));
-
-      task.value = "";
-      task.focus();
-    };
-    document.addEventListener("submit", submitHandler, false);
-    // document.addEventListener("render", renderHandler, false);
-    let app = document.querySelector("#app");
-    store.subscribe(() => {
-      app.innerHTML = template();
-    });
+    task.value = "";
+    task.focus();
   };
+  document.addEventListener("submit", submitHandler, false);
+})();
+
+export const TaskViewComponent = function () {
+  let state = store.getState();
+  let html = "";
+  const flatData = _.values(state);
+  let taskList = flatData.map((data, index) => {
+    return _.values(data).map((tsk) => {
+      const { id, task } = tsk;
+      return `
+    <li class="task" data-id=${task.id}>
+         <p class="has-task">
+           <span class="check-task-name">
+             <i class="ph-circle-bold icon-task-size"></i>
+             <span>${task.title}</span>
+           </span>
+           <span class="important-task">
+             <i class="ph-star-bold icon-task-size"></i>
+           </span>
+         </p>
+       </li>
+       `;
+    });
+  });
+
+  if (taskList.length > 0) {
+    let joined = taskList.join("").replaceAll(",", "");
+    html += `<ul class="tasks"> ${joined} </ul>`;
+  }
+
+  return html;
 };
-const render = TaskComponent();
-render();
