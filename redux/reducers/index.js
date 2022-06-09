f5d01ddbba66f2c590241648b411e9b0ecbd5d6c
farhdibehnamdev <behnammfarhadi@gmail.com>
2022-06-09 18:35:20 +0430

    🔥 refactor : Removed initialState from combineReducers.

--- redux/reducers/index.js
+++ redux/reducers/index.js
@@ -1,18 +1,11 @@
 import tasks from "./tasksReducer.js";
 import activeListId from "./activeListIdReducer.js";
 import list from "./listReducer.js";
-const initialState = {
-  activeListId: "Tasks-1",
-  list: { "Tasks-1": { id: "Tasks-1", title: "Tasks", tasksId: {} } },
-};
-console.log(initialState);
-const reducer = Redux.combineReducers(
-  {
-    tasks,
-    activeListId,
-    list,
-  },
-  initialState
-);
+
+const reducer = Redux.combineReducers({
+  tasks,
+  activeListId,
+  list,
+});
 
 export default reducer;
