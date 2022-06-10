e1f49bf564883e6b18a8f4969a05cf33ee00e5a2
farhdibehnamdev <behnammfarhadi@gmail.com>
2022-06-10 21:09:58 +0430

    ♻️ refactor : Refactored tasksId object to array.
    I need array structure to keep order of tasks add into it.

--- redux/reducers/listReducer.js
+++ redux/reducers/listReducer.js
@@ -7,36 +7,41 @@ const list = function (state = initialState, action) {
     case SET_TASK_INTO_LIST:
       const arrOfKeys = Object.keys(state);
       const loc = arrOfKeys.indexOf(action.payload.activeListId);
-      const listId = arrOfKeys[loc]; //Tasks-1
-
-      function isObject(item) {
-        return item && typeof item === "object" && !Array.isArray(item);
-      }
-      const mergeDeep = function (target, ...sources) {
-        if (!sources.length) return target;
-        const source = sources.shift();
-
-        if (isObject(target) && isObject(source)) {
-          for (const key in source) {
-            if (isObject(source[key])) {
-              if (!target[key]) Object.assign(target, { [key]: {} });
-              mergeDeep(target[key], source[key]);
-            } else {
-              Object.assign(target, { [key]: source[key] });
-            }
-          }
-        }
-
-        return mergeDeep(target, ...sources);
+      const listId = arrOfKeys[loc]; //List-1
+      const newTasksId = [...state[listId].tasksId, action.payload.taskId];
+      console.log(newTasksId);
+      const newState = {
+        ...state[arrOfKeys[loc]],
+        tasksId: newTasksId,
       };
+      // function isObject(item) {
+      //   return item && typeof item === "object" && !Array.isArray(item);
+      // }
+      // const mergeDeep = function (target, ...sources) {
+      //   if (!sources.length) return target;
+      //   const source = sources.shift();
 
-      const newObj = mergeDeep(state[arrOfKeys[loc]], {
-        tasksId: {
-          [action.payload.taskId]: { taskId: action.payload.taskId },
-        },
-      });
+      //   if (isObject(target) && isObject(source)) {
+      //     for (const key in source) {
+      //       if (isObject(source[key])) {
+      //         if (!target[key]) Object.assign(target, { [key]: {} });
+      //         mergeDeep(target[key], source[key]);
+      //       } else {
+      //         Object.assign(target, { [key]: source[key] });
+      //       }
+      //     }
+      //   }
 
-      return { ...state, [listId]: newObj };
+      //   return mergeDeep(target, ...sources);
+      // };
+
+      // const newObj = mergeDeep(state[arrOfKeys[loc]], {
+      //   tasksId: {
+      //     [action.payload.taskId]: { taskId: action.payload.taskId },
+      //   },
+      // });
+
+      return { ...state, [listId]: { ...newState } };
     default:
       return state;
   }
