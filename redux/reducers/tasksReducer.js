b878677258046fa3c94e78760f0b15613ffdc3cc
farhdibehnamdev <behnammfarhadi@gmail.com>
2022-06-12 17:06:24 +0430

    ✨ feat : each task has a important button.
    Users can mark tasks as important by clicking on the Star icon.

--- redux/reducers/tasksReducer.js
+++ redux/reducers/tasksReducer.js
@@ -1,9 +1,28 @@
-import { ADD_TASK } from "../constants/action-types.js";
+import { ADD_TASK, SET_TASK_IMPORTANT } from "../constants/action-types.js";
 const tasks = function (state = {}, action) {
-  if (action.type === ADD_TASK) {
-    return { ...state, [action.payload.id]: action.payload };
-  } else {
-    return state;
+  switch (action.type) {
+    case SET_TASK_IMPORTANT:
+      // console.log("state tasks important structure ::: ", state);
+      // const findKey = Object.keys(state).indexOf(action.payload.taskId);
+      const value = !state[action.payload].task.important;
+      console.log("important value ::::", value);
+
+      const setImportantValue = {
+        ...state[action.payload].task,
+        important: value,
+      };
+      console.log("setImportantValue ::: ", setImportantValue);
+      const newState = {
+        ...state[action.payload],
+        task: setImportantValue,
+      };
+
+      console.log("newState ::: ", newState);
+      return { ...state, [action.payload]: newState };
+    case ADD_TASK:
+      return { ...state, [action.payload.id]: action.payload };
+    default:
+      return state;
   }
 };
 
