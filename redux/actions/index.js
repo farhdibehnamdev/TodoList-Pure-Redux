b5521d9ec3d8c08a3f061720fe8a9005da6dc8b5
farhdibehnamdev <behnammfarhadi@gmail.com>
2022-06-12 16:57:45 +0430

    👔 Added actions for dealing with task status.

--- redux/actions/index.js
+++ redux/actions/index.js
@@ -1,4 +1,9 @@
-import { ADD_TASK, SET_TASK_INTO_LIST } from "../constants/action-types.js";
+import {
+  ADD_TASK,
+  SET_TASK_IMPORTANT,
+  SET_TASK_INTO_LIST,
+  SET_TASK_STATUS,
+} from "../constants/action-types.js";
 export const addTask = function (id, task) {
   return {
     type: ADD_TASK,
@@ -15,5 +20,19 @@ export const setTaskIntoList = function (taskId, activeListId) {
       taskId,
       activeListId,
     },
+  };
+};
+
+export const setTaskImportant = function (taskId) {
+  return {
+    type: SET_TASK_IMPORTANT,
+    payload: taskId,
+  };
+};
+
+export const setTaskStatus = function (taskStatus) {
+  return {
+    type: SET_TASK_STATUS,
+    payload: taskStatus,
   };
 };
