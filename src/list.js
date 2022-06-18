2d81da9b406a3fa46a173d1cea7a1a03a5e64d14
farhdibehnamdev <behnammfarhadi@gmail.com>
2022-06-18 19:16:19 +0430

    🐛 fix : Fixed bug display counts of tasks.
    When user click each tasks as completed. counts of tasks will change.

--- src/list.js
+++ src/list.js
@@ -1,7 +1,18 @@
 import store from "./redux/store";
 
 export const DefautListComponent = function () {
-  const { list, activeListId } = store.getState();
+  const state = store.getState();
   const listTasks = document.querySelector(".tasks .messages-count");
-  listTasks.innerHTML = list[activeListId].tasksId.length;
+  const tasks = state.list[state.activeListId].tasksId;
+  let counter = 0;
+  tasks.forEach((taskId) => {
+    const task = state.tasks[taskId].task;
+    task.completed === false ? counter++ : counter;
+  });
+  if (counter > 0) {
+    listTasks.classList.remove("hidden");
+    listTasks.innerHTML = counter ?? "";
+  } else {
+    listTasks.classList.add("hidden");
+  }
 };
