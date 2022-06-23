71505c43eededa9874da7565997e298eb75ecfe4
farhdibehnamdev <behnammfarhadi@gmail.com>
2022-06-23 14:25:33 +0430

    ♻️ refactor : Added a parameter to generatedId function
    Each new ID is prefixed with a prefix to separate tasks, lists, and categories.

--- src/libs/utils.js
+++ src/libs/utils.js
@@ -1,5 +1,7 @@
-export const generatedId = function () {
+export const generatedId = function (data = "newData") {
   return (
-    Math.random().toString(36).substring(2) + new Date().getTime().toString(36)
+    data +
+    Math.random().toString(36).substring(2) +
+    new Date().getTime().toString(36)
   );
 };
