95dc472dc7f67aaf91506614d1f3731b60a40805
farhdibehnamdev <97886945+farhdibehnamdev@users.noreply.github.com>
2022-07-12 10:37:24 +0430

    ✨ feat : Added toggle menu for task-details sidebar.

--- src/task-details.js
+++ src/task-details.js
@@ -4,6 +4,24 @@ const toggleTaskDetailsPanel = function (e) {
   taskDetails.classList.toggle("hidden");
 };
 
+const openMenu = function (e) {
+  console.log("hooo :::", e.target);
+  if (e.target.classList.contains("remind-me")) {
+    const remindMenu = document.querySelector(".remind-me-menu");
+    remindMenu && remindMenu.classList.toggle("hidden");
+  }
+};
+const closeOpenedMenu = function (e) {
+  if (!e.target.classList.contains("remind-me")) {
+    const menu = document.querySelector(".remind-me-menu");
+    if (!menu.classList.contains("hidden")) {
+      menu.classList.add("hidden");
+    }
+  }
+};
 (function () {
   appElement.addEventListener("click", toggleTaskDetailsPanel);
+  const taskDetailsDate = document.querySelector(".task-details-date");
+  taskDetailsDate.addEventListener("click", openMenu);
+  document.addEventListener("click", closeOpenedMenu);
 })();
